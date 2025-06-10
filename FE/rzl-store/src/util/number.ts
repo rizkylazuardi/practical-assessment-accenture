d81b416f8b4ce04969b1997280e4bea4343842d0
rizky lazuardi <rizky.lazuardi1@gmail.com>
2025-06-10 22:34:46 +0700

    deploy clean

--- FE/rzl-store/src/util/number.ts
+++
@@ -1,5 +0,0 @@
-const formatNumberWithComma = (num: number | null | undefined) => {
-    return num ? num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : 0;
-}
-
-export default formatNumberWithComma;
